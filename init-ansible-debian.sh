0d8c85b7882cfe9fcdc1df54997d609cc62a9c2c
Aurelien Legrand <aurelien.legrand01@gmail.com>
2017-06-04 12:34:40 -0700

    Improved init-ansible-debian.sh script.

--- init-ansible-debian.sh
+++ init-ansible-debian.sh
@@ -8,6 +8,6 @@ echo 'Defaults secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/s
 
 adduser ansible --shell /bin/bash --disabled-password --gecos ""
 mkdir -p /home/ansible/.ssh/
-chown ansible .ssh/
-echo $INITIAL_AUTHORIZED_KEY >> /home/ansible/.ssh/authorized_keys
+chown ansible /home/ansible/.ssh/
+grep -q "$INITIAL_AUTHORIZED_KEY" /home/ansible/.ssh/authorized_keys || echo $INITIAL_AUTHORIZED_KEY >> /home/ansible/.ssh/authorized_keys
 echo 'ansible ALL=(ALL) NOPASSWD:ALL' | tee -a /etc/sudoers
